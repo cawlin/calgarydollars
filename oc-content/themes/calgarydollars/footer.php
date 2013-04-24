0fbb12d48bcb8507f9e4b3205622835343c44515
calgaryd <calgaryd@Arushas-iMac.local>
2013-04-24 11:19:29 -0600

    add extra links to footer and fix dividers

--- oc-content/themes/calgarydollars/footer.php
+++ oc-content/themes/calgarydollars/footer.php
@@ -34,8 +34,10 @@
 	
     <div class="inner">
         <?php osc_reset_static_pages(); ?>
+		<a href="<?php echo osc_base_url(); ?>index.php?page=" class="first">Browse Listings</a> |
+		<a href="<?php echo osc_base_url(); ?>/calgary-dollars-business-directory">Calgary Dollars Business Directory</a>
         <?php while( osc_has_static_pages() ) { ?>
-			<a href="<?php echo osc_static_page_url(); ?>"><?php echo osc_static_page_title(); ?></a>
+			| <a href="<?php echo osc_static_page_url(); ?>"><?php echo osc_static_page_title(); ?></a>
         <?php } ?>
     </div>
 </div>
